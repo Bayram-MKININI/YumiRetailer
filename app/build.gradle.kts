6574f03f61a920cf5885968bf984da2033e4fb19
Bayram M'KININI <bayram_mkinini.external@discovery.com>
2023-08-29 21:10:37 +0100

    FEAT: Extend splash screen duration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 9
-        versionName = "1.0"
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
