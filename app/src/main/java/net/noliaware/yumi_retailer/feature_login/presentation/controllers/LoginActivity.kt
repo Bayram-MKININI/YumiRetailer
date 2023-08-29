6bb44288f619585173eeb9e9fcca59a91888054f
Bayram M'KININI <bayram_mkinini.external@discovery.com>
2023-08-29 14:12:39 +0100

    FEAT: Extend splash screen duration

--- app/src/main/java/net/noliaware/yumi_retailer/feature_login/presentation/controllers/LoginActivity.kt
+++ app/src/main/java/net/noliaware/yumi_retailer/feature_login/presentation/controllers/LoginActivity.kt
@@ -1,6 +1,7 @@
 package net.noliaware.yumi_retailer.feature_login.presentation.controllers
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -9,9 +10,16 @@ import net.noliaware.yumi_retailer.R
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
 
+    private val viewModel: LoginActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value
+            }
+        }
+
         super.onCreate(savedInstanceState)
-        installSplashScreen()
         setContentView(R.layout.activity_login)
         supportFragmentManager.beginTransaction().run {
             replace(R.id.main_fragment_container, LoginFragment())
