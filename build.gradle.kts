2f4d56fdb8ef4d9a6a4e1fa188051a4da83ae3fd
Bayram M'KININI <bayram_mkinini.external@discovery.com>
2023-12-20 16:37:24 +0100

    FIX: Fix messaging refresh

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("com.android.library") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
     id("com.google.devtools.ksp") version "1.9.21-1.0.15" apply false
     id("com.google.dagger.hilt.android") version "2.49" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
